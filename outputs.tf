debc7fab28de120ceb250e26d71ba4f19a1cb447
harry.sarshogh <harry.sarshogh@gmail.com>
2026-05-26 22:46:25 +0100

    feat: implement tf-atom-apigateway-deployment-aws module

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,14 @@
 output "enabled" {
-  description = "Whether the module is enabled."
+  description = "Whether the module is enabled"
   value       = local.enabled
+}
+
+output "id" {
+  description = "ID of the deployment"
+  value       = try(aws_api_gateway_deployment.this[0].id, null)
+}
+
+output "execution_arn" {
+  description = "Execution ARN of the deployment"
+  value       = try(aws_api_gateway_deployment.this[0].execution_arn, null)
 }
